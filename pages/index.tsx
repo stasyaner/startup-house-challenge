0461cc448362d750fbd49a728110637ce30a575a
stasyaner <stasyaner@hotmail.com>
2021-10-09 12:44:28 +0200

    create base next.js app

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Stock Dashboard</title>
+            </Head>
+            <div>hello world</div>
+        </>
+    );
+};
+
+export default Home;
